5eab39ab973d59522453a5f4e99a02d68168f75b
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2024-12-23 20:06:32 +0100

    Client app B2C add scope for Server app

--- source/ChatApp.Web/Program.cs
+++ source/ChatApp.Web/Program.cs
@@ -8,10 +8,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+var accessTokenScopeUri = builder.Configuration["AccessTokenScopeUri"] ?? "";
+
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
     options.ProviderOptions.LoginMode = "redirect";
+    options.ProviderOptions.DefaultAccessTokenScopes.Add(accessTokenScopeUri);
 });
 
 await builder.Build().RunAsync();
