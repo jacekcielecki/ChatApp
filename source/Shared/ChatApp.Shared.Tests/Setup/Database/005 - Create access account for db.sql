b8333f310b4cb08bdc080439d61f9d0b5cac10ad
Jacek Cielecki <jcielecki@sii.pl>
2025-08-25 00:34:56 +0200

    add sql

--- source/Shared/ChatApp.Shared.Tests/Setup/Database/005 - Create access account for db.sql
+++ source/Shared/ChatApp.Shared.Tests/Setup/Database/005 - Create access account for db.sql
@@ -1,2 +1,15 @@
 CREATE USER developer_access WITH PASSWORD 'secret_password';
-GRANT ALL PRIVILEGES ON DATABASE "chatapp-sql" TO developer_access;
+GRANT CONNECT ON DATABASE "chatapp-sql" TO developer_access;
+
+-- Tables
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO developer_access;
+
+-- Sequences
+GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO developer_access;
+
+-- Future tables & sequences
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO developer_access;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO developer_access;
